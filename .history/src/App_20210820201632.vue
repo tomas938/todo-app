138ec44eaef8466ae853b246a5ba7f6da53ae291
tomas938 <tomasalbert9387182@gmail.com>
2021-08-20 22:27:59 +0200

    🤩🤩 final test

---
+++ .history/src/App_20210820201632.vue
@@ -0,0 +1,49 @@
+<template>
+	<router-view />
+	<img
+		src="/src/assets/280-2800290_ice-age-squirrel-png-ice-age-scrat-png.png"
+		alt=""
+	/>
+</template>
+
+<style lang="scss">
+@import "/src/scss/_variables";
+:root {
+	.dark {
+		--bg-image: url(./assets/bg-desktop-dark.jpg);
+		--items-bg-color: hsl(235, 24%, 19%);
+		--body-bg: hsl(235, 21%, 11%);
+		--todo-text: hsl(236, 33%, 92%);
+		--todo-border: hsl(237, 14%, 26%);
+	}
+	.light {
+		--bg-image: url(./assets/bg-desktop-light.jpg);
+		--items-bg-color: hsl(0, 0%, 100%);
+		--body-bg: hsl(0, 0%, 98%);
+		--todo-text: hsl(233, 14%, 35%);
+		--todo-border: hsl(236, 33%, 92%);
+	}
+}
+
+* {
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
+	// transition: all 0.4s ease-in-out;
+}
+html {
+	font-size: 62.5%;
+}
+body {
+	background-image: url(/src/assets/280-2800290_ice-age-squirrel-png-ice-age-scrat-png.png);
+	background-position: bottom;
+	font-size: 1.8rem;
+	font-family: $font-primary;
+	background-color: var(--body-bg);
+	transition: all 0.4s ease-in-out;
+}
+
+li {
+	list-style: none;
+}
+</style>
---
+++ .history/src/components/Header_20210820205410.vue
@@ -0,0 +1,176 @@
+<template>
+	<header>
+		<nav class="nav container">
+			<div class="nav__title">
+				<h1>todo</h1>
+			</div>
+			<svg
+				v-if="ligth"
+				@click="changeTheme"
+				class="nav__theme"
+				xmlns="http://www.w3.org/2000/svg"
+				width="26"
+				height="26"
+			>
+				<path
+					fill="#fafafa"
+					fill-rule="evenodd"
+					d="M13 0c.81 0 1.603.074 2.373.216C10.593 1.199 7 5.43 7 10.5 7 16.299 11.701 21 17.5 21c2.996 0 5.7-1.255 7.613-3.268C23.22 22.572 18.51 26 13 26 5.82 26 0 20.18 0 13S5.82 0 13 0z"
+				/>
+			</svg>
+			<svg
+				v-else
+				@click="changeTheme"
+				xmlns="http://www.w3.org/2000/svg"
+				width="26"
+				height="26"
+			>
+				<path
+					fill="#fafafa"
+					fill-rule="evenodd"
+					d="M13 21a1 1 0 011 1v3a1 1 0 11-2 0v-3a1 1 0 011-1zm-5.657-2.343a1 1 0 010 1.414l-2.121 2.121a1 1 0 01-1.414-1.414l2.12-2.121a1 1 0 011.415 0zm12.728 0l2.121 2.121a1 1 0 01-1.414 1.414l-2.121-2.12a1 1 0 011.414-1.415zM13 8a5 5 0 110 10 5 5 0 010-10zm12 4a1 1 0 110 2h-3a1 1 0 110-2h3zM4 12a1 1 0 110 2H1a1 1 0 110-2h3zm18.192-8.192a1 1 0 010 1.414l-2.12 2.121a1 1 0 01-1.415-1.414l2.121-2.121a1 1 0 011.414 0zm-16.97 0l2.121 2.12A1 1 0 015.93 7.344L3.808 5.222a1 1 0 011.414-1.414zM13 0a1 1 0 011 1v3a1 1 0 11-2 0V1a1 1 0 011-1z"
+				/>
+			</svg>
+		</nav>
+		<div class="new-todo">
+			<input
+				autofocus
+				type="text"
+				placeholder="Create a new todo..."
+				v-model="newTodo"
+				@keypress.enter="$emit('create-todo', newTodo), clearInput()"
+			/>
+		</div>
+	</header>
+	<div class="img">
+		<!-- <img src="../assets/image1.png" alt="" /> -->
+	</div>
+	<div class="img2">
+		<!-- <img src="../assets/image1.png" alt="" /> -->
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			newTodo: "",
+			ligth: true,
+			body: document.body,
+		};
+	},
+	methods: {
+		changeTheme() {
+			this.ligth = !this.ligth;
+			if (!this.ligth) {
+				this.body.classList.remove("light");
+				this.body.classList.add("dark");
+			} else {
+				this.body.classList.remove("dark");
+				this.body.classList.add("light");
+			}
+		},
+		clearInput() {
+			this.newTodo = "";
+		},
+	},
+	mounted() {
+		this.body.classList.add("light");
+	},
+};
+</script>
+
+<style lang="scss" scoped>
+@import "/src/scss/_variables";
+.img {
+	position: absolute;
+	left: 0;
+	bottom: 0;
+	background-image: url(../assets/ice.png);
+	background-repeat: no-repeat;
+	width: 100%;
+	height: 100%;
+}
+.img2 {
+	position: absolute;
+	right: 0;
+	bottom: 0;
+	background-image: url(../assets/Sid.png);
+	background-repeat: no-repeat;
+	background-position: center;
+	width: 30rem;
+	height: 30rem;
+}
+header {
+	height: 30rem;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	gap: 5rem;
+	padding: 3rem;
+	background-image: var(--bg-image);
+	background-repeat: no-repeat;
+	background-size: cover;
+}
+.nav {
+	max-width: 67.5rem;
+	width: 100%;
+	display: flex;
+	justify-content: space-between;
+	align-items: baseline;
+	&__title h1 {
+		font-size: 4.5rem;
+		letter-spacing: 1.8rem;
+		@media only screen and (max-width: 500px) {
+			font-size: 3rem;
+			letter-spacing: 1.5rem;
+		}
+		text-transform: uppercase;
+	}
+
+	svg {
+		cursor: pointer;
+	}
+}
+.new-todo {
+	max-width: 67.5rem;
+	width: 100%;
+	position: relative;
+	&::before {
+		content: "";
+		position: absolute;
+		top: 50%;
+		transform: translateY(-50%);
+		left: 2.5rem;
+		width: 3rem;
+		height: 3rem;
+		border-radius: 50%;
+		border: 1px solid var(--todo-border);
+	}
+}
+input {
+	background-color: var(--items-bg-color);
+	color: var(--todo-text);
+	width: 100%;
+	outline: none;
+	border: none;
+	padding: 2rem;
+	padding-left: 8rem;
+	border-radius: 0.5rem;
+	font-size: 1.8rem;
+	@media only screen and (max-width: 350px) {
+		font-size: 1.4rem;
+		padding-left: 7rem;
+	}
+	font-family: $font-primary;
+	font-weight: $light;
+}
+h1 {
+	color: $light-gray;
+}
+header,
+input {
+	transition: all 0.4s ease-in-out;
+}
+</style>
